e510b1d1c57d7d9eb749f5ebcc76dee2301f754d
hisondev <64723261+hisondev@users.noreply.github.com>
2023-11-17 17:07:18 +0900

    Update DataWrapperSerializer.java

--- data-model/src/main/java/io/github/hison0319/data/DataWrapperSerializer.java
+++ data-model/src/main/java/io/github/hison0319/data/DataWrapperSerializer.java
@@ -5,25 +5,27 @@ import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
 import java.io.IOException;
-import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
 
 public class DataWrapperSerializer extends JsonSerializer<DataWrapper> {
 
     @Override
     public void serialize(DataWrapper value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
         gen.writeStartObject();
+        
+        HashMap<String, Object> data = value.getDatas();
 
-        for (Map.Entry<String, String> entry : value.getStrings().entrySet()) {
-            if (entry.getValue() != null) {
-                gen.writeStringField(entry.getKey(), entry.getValue());
-            } else {
-                gen.writeNullField(entry.getKey());
+        Set<String> keys = data.keySet();
+        for (String key : keys) {
+            if (data.get(key) == null) {
+                gen.writeNullField(key);
+            } else if (data.get(key) instanceof String) {
+                gen.writeStringField(key, (String)data.get(key));
+            } else if (data.get(key) instanceof DataModelBase) {
+                gen.writeFieldName(key);
+                gen.writeTree(((DataModelBase)data.get(key)).getConvertJson());
             }
-        }
-
-        for (Map.Entry<String, DataModelBase> entry : value.getDataModels().entrySet()) {
-            gen.writeFieldName(entry.getKey());
-            gen.writeTree(entry.getValue().getConvertJson());
         }
 
         gen.writeEndObject();
