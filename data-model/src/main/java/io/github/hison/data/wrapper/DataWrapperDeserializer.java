a9cd6e180cd69d180fb8ef4169bd16f2facdcf6c
hisondev <64723261+hisondev@users.noreply.github.com>
2023-11-23 12:54:15 +0900

    Update DataWrapperDeserializer.java

--- data-model/src/main/java/io/github/hison/data/wrapper/DataWrapperDeserializer.java
+++ data-model/src/main/java/io/github/hison/data/wrapper/DataWrapperDeserializer.java
@@ -1,6 +1,6 @@
-package com.example.demo.common.data.wrapper;
+package io.github.hison.data.model.wrapper;
 
-package io.github.hison.data.model.DataModel;
+import com.example.demo.common.data.model.DataModel;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
