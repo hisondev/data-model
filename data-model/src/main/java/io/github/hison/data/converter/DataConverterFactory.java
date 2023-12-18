10582ae501b5ff239045ea70f97afa71809b9e9f
hison0319 <hison0319@gmail.com>
2023-12-18 22:25:21 +0900

    remove dependencies

---
+++ data-model/src/main/java/io/github/hison/data/converter/DataConverterFactory.java
@@ -0,0 +1,17 @@
+package io.github.hison.data.converter;
+
+public class DataConverterFactory {
+
+    private static DataConverter customConverter;
+    
+    public static DataConverter getConverter() {
+        if (customConverter != null) {
+            return customConverter;
+        }
+        return new DataConverterDefault();
+    }
+
+    public static void setCustomConverter(DataConverter converter) {
+        customConverter = converter;
+    }
+}
